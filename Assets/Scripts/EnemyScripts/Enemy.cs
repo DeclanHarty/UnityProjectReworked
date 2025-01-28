27cf83a9f858b9a1d51b67044cf3cb1e5f1bc1c9
DeclanHarty <declanharty04@gmail.com>
2025-01-28 09:15:14 -0500

    Added basic enemy scripts

---
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyInfo enemyInfo;
+
+    public int currentHealth;
+
+    public void Awake(){
+        currentHealth = enemyInfo.maxHealth;
+    }
+    public void TakeDamage(int damage){
+        currentHealth -= damage;
+        if(currentHealth <= 0){
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Items/HeldItems/Gun.cs
+++ Assets/Scripts/Items/HeldItems/Gun.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Gun : HeldItem
 {
+    public int damage = 8;
 
     public override void Fire1Action()
     {
         RaycastHit2D hit = Physics2D.Raycast(currentPosition, mouseDirection, 10, LayerMask.GetMask(new string[] {"Ground","Enemy"}));
         if(hit){
             Debug.Log(hit.collider.gameObject.layer);
+            if(hit.collider.gameObject.layer == 8){
+                hit.collider.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+            }
             Debug.DrawLine(currentPosition, hit.distance * mouseDirection + currentPosition, Color.red, .2f);
         }
         
